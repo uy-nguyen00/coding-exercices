09ed6fb4fb499e6523e308d4d919705959edff5f
Uy Nguyen <uynguyen1501@gmail.com>
2025-07-07 16:50:39 +0200

    LeetCode 138 - Copy List with Random Pointer

---
+++ LeetCode/LeetCode138_Copy_List_with_Random_Pointer.java
@@ -0,0 +1,74 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+import java.util.*;
+
+class LeetCode138_Copy_List_with_Random_Pointer {
+    public static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public static Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Map<Node, Node> map = new HashMap<>();
+
+        Node current = head;
+        while (current != null) {
+            map.put(current, new Node(current.val));
+            current = current.next;
+        }
+
+        current = head;
+        while (current != null) {
+            Node copyNode = map.get(current);
+            copyNode.next = map.get(current.next);
+            copyNode.random = map.get(current.random);
+            current = current.next;
+        }
+
+        return map.get(head);
+    }
+
+    public static void main(String[] args) {
+        Node node0 = new Node(7);
+        Node node1 = new Node(13);
+        Node node2 = new Node(11);
+        Node node3 = new Node(10);
+        Node node4 = new Node(1);
+
+        node0.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        node1.random = node0;
+        node2.random = node4;
+        node3.random = node2;
+        node4.random = node0;
+
+        System.out.println(copyRandomList(node0));
+    }
+}
