4993c52ac4151ec7b9a991ece11c0bad0ed2e7a1
Uy Nguyen <uynguyen1501@gmail.com>
2025-07-08 23:09:57 +0200

    LeetCode 1603 - Design Parking System

---
+++ LeetCode/LeetCode1603_Design_Parking_System.java
@@ -0,0 +1,17 @@
+class LeetCode1603_Design_Parking_System {
+    private int[] slots = new int[3];
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.slots = new int[]{big, medium, small};
+    }
+
+    public boolean addCar(int carType) {
+        return this.slots[carType - 1]-- > 0;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
