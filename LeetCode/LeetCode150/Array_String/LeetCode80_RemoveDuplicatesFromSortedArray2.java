0f451e813455406bf6f518fa0c3b7b8d6ee07567
Uy Nguyen <uynguyen1501@gmail.com>
2026-01-18 18:29:16 +0100

    LeetCode 80 - Remove Duplicates from Sorted Array II

---
+++ LeetCode/LeetCode150/Array_String/LeetCode80_RemoveDuplicatesFromSortedArray2.java
@@ -0,0 +1,13 @@
+package LeetCode150.Array_String;
+
+public class LeetCode80_RemoveDuplicatesFromSortedArray2 {
+    public int removeDuplicates(int[] nums) {
+        int insertIndex = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[insertIndex - 2] != nums[i]) {
+                nums[insertIndex++] = nums[i];
+            }
+        }
+        return insertIndex;
+    }
+}
