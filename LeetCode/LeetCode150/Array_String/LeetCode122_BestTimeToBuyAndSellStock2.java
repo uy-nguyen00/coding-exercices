4f63b9d868970f62a0170e90c88b4442804664cd
Uy Nguyen <uynguyen1501@gmail.com>
2026-01-21 22:57:17 +0100

    LeetCode 122: Best Time to Buy and Sell Stock II - add another solution

--- LeetCode/LeetCode150/Array_String/LeetCode122_BestTimeToBuyAndSellStock2.java
+++ LeetCode/LeetCode150/Array_String/LeetCode122_BestTimeToBuyAndSellStock2.java
@@ -21,4 +21,21 @@ public class LeetCode122_BestTimeToBuyAndSellStock2 {
         }
         return maxprofit;
     }
+
+    /**
+     * A simpler version of Peak Valley approach
+     * Time complexity: O(N)
+     * Space complexity: O(1)
+     */
+    public int maxProfit2(int[] prices) {
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return maxProfit;
+    }
 }
