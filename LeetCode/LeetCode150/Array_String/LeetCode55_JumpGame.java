38147a29493a0a347509dd963f52b55e85532326
Uy Nguyen <uynguyen1501@gmail.com>
2026-01-27 09:36:56 +0100

    LeetCode 55 - Jump Game - add greedy approach

--- LeetCode/LeetCode150/Array_String/LeetCode55_JumpGame.java
+++ LeetCode/LeetCode150/Array_String/LeetCode55_JumpGame.java
@@ -113,4 +113,22 @@ public class LeetCode55_JumpGame {
             return memo[0] != null && memo[0] == true;
         }
     }
+
+    /**
+     * Greedy Approach
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     */
+    class Solution4 {
+
+        public boolean canJump(int[] nums) {
+            int lastPos = nums.length - 1;
+            for (int i = lastPos; i >= 0; i--) {
+                if (i + nums[i] >= lastPos) {
+                    lastPos = i;
+                }
+            }
+            return lastPos == 0;
+        }
+    }
 }
