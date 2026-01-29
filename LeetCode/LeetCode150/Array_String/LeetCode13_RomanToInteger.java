49ddfb155a047ed627507d953f66e533c0301172
Uy Nguyen <uynguyen1501@gmail.com>
2026-01-29 21:33:34 +0100

    LeetCode 13 - Roman To Integer

---
+++ LeetCode/LeetCode150/Array_String/LeetCode13_RomanToInteger.java
@@ -0,0 +1,44 @@
+package LeetCode150.Array_String;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode13_RomanToInteger {
+
+    static Map<String, Integer> values = new HashMap<>();
+
+    static {
+        values.put("M", 1000);
+        values.put("D", 500);
+        values.put("C", 100);
+        values.put("L", 50);
+        values.put("X", 10);
+        values.put("V", 5);
+        values.put("I", 1);
+    }
+
+    /**
+     * Right-to-Left Pass without worrying about end of string.
+     * Time complexity: O(1)
+     * Space complexity: O(1)
+     */
+
+    public int romanToInt(String s) {
+        String lastSymbol = s.substring(s.length() - 1);
+        int lastValue = values.get(lastSymbol);
+        int total = lastValue;
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+            String currentSymbol = s.substring(i, i + 1);
+            int currentValue = values.get(currentSymbol);
+            if (currentValue < lastValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+            lastValue = currentValue;
+        }
+
+        return total;
+    }
+}
