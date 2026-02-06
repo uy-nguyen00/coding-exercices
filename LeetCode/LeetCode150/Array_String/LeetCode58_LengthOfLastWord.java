36145a7141300ebe24ad186ec396d180b423df73
Uy Nguyen <uynguyen1501@gmail.com>
2026-02-06 20:10:42 +0100

    LeetCode 58 - add more solutions

--- LeetCode/LeetCode150/Array_String/LeetCode58_LengthOfLastWord.java
+++ LeetCode/LeetCode150/Array_String/LeetCode58_LengthOfLastWord.java
@@ -16,4 +16,56 @@ public class LeetCode58_LengthOfLastWord {
             return s.length() - s.lastIndexOf(" ") - 1;
         }
     }
+
+    /**
+     * String Index Manipulation with 2 loops
+     *
+     * Time complexity: O(N)
+     *
+     * Space complexity: O(1)
+     */
+    class Solution2 {
+
+        public int lengthOfLastWord(String s) {
+            // Remove trailing spaces
+            int p = s.length() - 1;
+            while (p >= 0 && s.charAt(p) == ' ') {
+                p--;
+            }
+
+            int length = 0;
+            while (p >= 0 && s.charAt(p) != ' ') {
+                p--;
+                length++;
+            }
+
+            return length;
+        }
+    }
+
+    /**
+     * One loop approach
+     *
+     * Time complexity: O(N)
+     *
+     * Space complexity: O(1)
+     */
+    class Solution3 {
+
+        public int lengthOfLastWord(String s) {
+            int p = s.length() - 1;
+            int length = 0;
+
+            while (p >= 0) {
+                if (s.charAt(p) != ' ') {
+                    length++;
+                } else if (length > 0) {
+                    return length;
+                }
+                p--;
+            }
+
+            return length;
+        }
+    }
 }
