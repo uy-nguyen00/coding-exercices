9f54001648b1db915642ff56fd92f0cd32fa4fea
Uy Nguyen <uynguyen1501@gmail.com>
2026-02-08 23:13:19 +0100

    LeetCode 383 - add solution and fix complexities

--- LeetCode/LeetCode150/Array_String/LeetCode383_RansomNote.java
+++ LeetCode/LeetCode150/Array_String/LeetCode383_RansomNote.java
@@ -8,13 +8,17 @@ public class LeetCode383_RansomNote {
     /**
      * Using HashMap
      *
-     * Time complexity: O(R + M)
+     * Time complexity: O(R + M) = O(M), since M > R
      *
-     * Space complexity: O(M)
+     * Space complexity: O(k) = O(1), since k <= 26
      */
     class Solution1 {
 
         public boolean canConstruct(String ransomNote, String magazine) {
+            if (ransomNote.length() > magazine.length()) {
+                return false;
+            }
+
             Map<Character, Integer> charCount = new HashMap<>();
 
             for (char c : magazine.toCharArray()) {
@@ -25,6 +29,34 @@ public class LeetCode383_RansomNote {
                 int count = charCount.getOrDefault(c, 0);
                 if (count == 0) return false;
                 charCount.put(c, count - 1);
+            }
+
+            return true;
+        }
+    }
+
+    /**
+     * Using fixed-size array
+     *
+     * Time complexity: O(M)
+     *
+     * Space complexity: O(1)
+     */
+    class Solution2 {
+
+        public boolean canConstruct(String ransomNote, String magazine) {
+            if (ransomNote.length() > magazine.length()) {
+                return false;
+            }
+
+            int[] charCount = new int[26];
+
+            for (char c : magazine.toCharArray()) {
+                charCount[c - 'a']++;
+            }
+
+            for (char c : ransomNote.toCharArray()) {
+                if (--charCount[c - 'a'] < 0) return false;
             }
 
             return true;
