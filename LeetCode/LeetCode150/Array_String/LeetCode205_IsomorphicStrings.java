823a02be102efbf285aaba3b9fc8cfa0c63b93e4
Uy Nguyen <uynguyen1501@gmail.com>
2026-02-09 23:24:27 +0100

    LeetCode 205 - Isomorphic Strings

---
+++ LeetCode/LeetCode150/Array_String/LeetCode205_IsomorphicStrings.java
@@ -0,0 +1,44 @@
+package LeetCode150.Array_String;
+
+import java.util.Arrays;
+
+public class LeetCode205_IsomorphicStrings {
+
+    /**
+     * Using 2 arrays
+     *
+     * Time complexity: O(N)
+     *
+     * Space complexity: O(1)
+     */
+    class Solution1 {
+
+        public boolean isIsomorphic(String s, String t) {
+            int[] mappingDictStoT = new int[256];
+            Arrays.fill(mappingDictStoT, -1);
+
+            int[] mappingDictTtoS = new int[256];
+            Arrays.fill(mappingDictTtoS, -1);
+
+            for (int i = 0; i < s.length(); ++i) {
+                char c1 = s.charAt(i);
+                char c2 = t.charAt(i);
+
+                // Case 1: No mapping exists in either of the dictionaries
+                if (mappingDictStoT[c1] == -1 && mappingDictTtoS[c2] == -1) {
+                    mappingDictStoT[c1] = c2;
+                    mappingDictTtoS[c2] = c1;
+                }
+                // Case 2: Either mapping doesn't exist in one of the dictionaries or Mapping exists and
+                // it doesn't match in either of the dictionaries or both
+                else if (
+                    !(mappingDictStoT[c1] == c2 && mappingDictTtoS[c2] == c1)
+                ) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
