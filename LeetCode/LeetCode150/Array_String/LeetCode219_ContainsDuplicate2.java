3e92eb12e3465ad6b436679fd885603569a15fba
Uy Nguyen <uynguyen1501@gmail.com>
2026-02-17 19:34:33 +0100

    LeetCode 219 - add solution with sliding window

--- LeetCode/LeetCode150/Array_String/LeetCode219_ContainsDuplicate2.java
+++ LeetCode/LeetCode150/Array_String/LeetCode219_ContainsDuplicate2.java
@@ -1,7 +1,9 @@
 package LeetCode150.Array_String;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class LeetCode219_ContainsDuplicate2 {
 
@@ -12,16 +14,42 @@ public class LeetCode219_ContainsDuplicate2 {
      *
      * Space complexity: O(N)
      */
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (
-                map.containsKey(nums[i]) && Math.abs(map.get(nums[i]) - i) <= k
-            ) {
-                return true;
+    class Solution1 {
+
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (
+                    map.containsKey(nums[i]) &&
+                    Math.abs(map.get(nums[i]) - i) <= k
+                ) {
+                    return true;
+                }
+                map.put(nums[i], i);
             }
-            map.put(nums[i], i);
+            return false;
         }
-        return false;
+    }
+
+    /**
+     * Using Set with sliding window
+     *
+     * Time copmlexity: O(N)
+     *
+     * Space complexity: O(min(N, k))
+     */
+    class Solution2 {
+
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            Set<Integer> set = new HashSet<>();
+            for (int i = 0; i < nums.length; ++i) {
+                if (set.contains(nums[i])) return true;
+                set.add(nums[i]);
+                if (set.size() > k) {
+                    set.remove(nums[i - k]);
+                }
+            }
+            return false;
+        }
     }
 }
