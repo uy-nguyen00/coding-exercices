32c1792959384fee1dd76c7f3da17b197573b540
Uy Nguyen <uynguyen1501@gmail.com>
2026-02-20 15:54:14 +0100

    LeetCode 202 - Happy Number - add Floyd's cycle finding algo

--- LeetCode/LeetCode150/Array_String/LeetCode202_HappyNumber.java
+++ LeetCode/LeetCode150/Array_String/LeetCode202_HappyNumber.java
@@ -33,4 +33,36 @@ public class LeetCode202_HappyNumber {
             return n == 1;
         }
     }
+
+    /**
+     * Floyd's Cycle Finding Algorithm
+     *
+     * Time complexity: O(log N)
+     *
+     * Space complexity: O(1)
+     */
+    class Solution2 {
+
+        private int getNext(int n) {
+            int totalSum = 0;
+            int d = n % 10;
+            while (n > 0) {
+                n = n / 10;
+                totalSum += d * d;
+            }
+            return totalSum;
+        }
+
+        public boolean isHappy(int n) {
+            int slowRunner = n;
+            int fastRunner = getNext(n);
+
+            while (slowRunner != fastRunner && fastRunner != 1) {
+                slowRunner = getNext(slowRunner);
+                fastRunner = getNext(getNext(fastRunner));
+            }
+
+            return fastRunner == 1;
+        }
+    }
 }
