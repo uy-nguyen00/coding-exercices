4a22d995025d97599d0109ab47e258f111abfa0e
Uy Nguyen <uynguyen1501@gmail.com>
2026-02-27 22:55:11 +0100

    LeetCode 226 - Invert Binary Tree - add DFS solution

--- LeetCode/LeetCode150/Array_String/LeetCode226_InvertBinaryTree.java
+++ LeetCode/LeetCode150/Array_String/LeetCode226_InvertBinaryTree.java
@@ -1,5 +1,8 @@
 package LeetCode150.Array_String;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class LeetCode226_InvertBinaryTree {
 
     class TreeNode {
@@ -38,6 +41,35 @@ public class LeetCode226_InvertBinaryTree {
 
             root.left = right;
             root.right = left;
+
+            return root;
+        }
+    }
+
+    /**
+     * Iteration with DFS
+     *
+     * Time complexity: O(N)
+     *
+     * Space complexity: O(N)
+     */
+    class Solution2 {
+
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) return null;
+
+            Deque<TreeNode> stack = new ArrayDeque<>();
+            stack.push(root);
+
+            while (!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                TreeNode temp = node.left;
+                node.left = node.right;
+                node.right = temp;
+
+                if (node.left != null) stack.push(node.left);
+                if (node.right != null) stack.push(node.right);
+            }
 
             return root;
         }
