2fe3d70e45a9549bf16870a5a6f0c3d600c2f7f2
Uy Nguyen <uynguyen1501@gmail.com>
2026-03-01 21:22:57 +0100

    LeetCode 101 - Symmetric Tree - add iterative solution

--- LeetCode/LeetCode150/Array_String/LeetCode101_SymmetricTree.java
+++ LeetCode/LeetCode150/Array_String/LeetCode101_SymmetricTree.java
@@ -1,5 +1,8 @@
 package LeetCode150.Array_String;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LeetCode101_SymmetricTree {
 
     class TreeNode {
@@ -43,6 +46,34 @@ public class LeetCode101_SymmetricTree {
         public boolean isSymmetric(TreeNode root) {
             if (root == null) return true;
             return isMirror(root.left, root.right);
+        }
+    }
+
+    /**
+     * Iteration
+     *
+     * Time complexity: O(N)
+     *
+     * Space complexity: O(N)
+     */
+    class Solution2 {
+
+        public boolean isSymmetric(TreeNode root) {
+            Queue<TreeNode> q = new LinkedList<>();
+            q.add(root);
+            q.add(root);
+            while (!q.isEmpty()) {
+                TreeNode t1 = q.poll();
+                TreeNode t2 = q.poll();
+                if (t1 == null && t2 == null) continue;
+                if (t1 == null || t2 == null) return false;
+                if (t1.val != t2.val) return false;
+                q.add(t1.left);
+                q.add(t2.right);
+                q.add(t1.right);
+                q.add(t2.left);
+            }
+            return true;
         }
     }
 }
